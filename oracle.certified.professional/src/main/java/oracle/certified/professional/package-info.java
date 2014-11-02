695eceb291b7ede2a9ed0174eb8ee085c41be27e
Marek Panek <marek.panek@o2.pl>
2014-11-02 18:31:05 +0100

    OCP - Initial commit.

---
+++ oracle.certified.professional/src/main/java/oracle/certified/professional/package-info.java
@@ -0,0 +1,19 @@
+/**
+ * Oracle Certified Professional, Java SE 7 Programmer (1Z0-804) exam objectives.
+ * 
+ *  <pre>
+ *  +--------------------------------------------------------------------------------------------------+
+ *  |                                  Java SE 7 Programmer II                                         |
+ *  +---------------------------------------+----------------------------------------------------------+
+ *  |   Exam Number                         |   1Z0-804                                                |
+ *  |   Associated Certifications           |   Oracle Certified Professional, Java SE 7 Programmer    |
+ *  |   Duration                            |   150 minutes                                            |
+ *  |   Number of Questions                 |   90                                                     |
+ *  |   Passing Score                       |   65%                                                    |
+ *  |   Format                              |   Multiple Choice                                        |
+ *  +---------------------------------------+----------------------------------------------------------+
+ *  </pre>
+ *
+ * @author mpanek
+ */
+package oracle.certified.professional;
